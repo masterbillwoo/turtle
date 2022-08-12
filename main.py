1196c6dad15151edbba6a168ea4fe0b9b86047d3
justinmahgeorgebrownyes <justin.mah2@georgebrown.ca>
2022-08-12 10:25:02 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import turtle as t
+
+timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.backward(200)
+timmy_the_turtle.right(90)
+timmy_the_turtle.left(180)
+timmy_the_turtle.setheading(0)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
